52d98eff5091456876e82178ae4d7f9c7b3f9cda
Daniel Gray <the.daniel.gray@gmail.com>
2020-05-20 17:42:22 +0200

    Add IP echo server in Mojolicious / Perl

---
+++ ip_echo_server.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/env perl
+
+use Mojolicious::Lite;
+
+get '/ipcheck' => sub {
+	my $c = shift;
+	my $ip = $c->tx->remote_address;
+	$c->render(text => "$ip\n");
+};
+any '/' => {text => ''};
+any '/*somepath' => {text => ''};
+
+app->hook(after_dispatch => sub {
+  my $c = shift;
+  $c->res->headers->remove('Server');
+});
+
+app->start;
+__DATA__
+
+@@ exception.html.ep
+<!DOCTYPE html>
+<html>
+  <head><title>Server error</title></head>
+  <body>
+	  <p>Please come back later.</p>
+  </body>
+</html>
